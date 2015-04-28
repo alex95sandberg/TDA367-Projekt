90498769c3636625d3106cea2a9e4712064eefb3
Chewii <rasmusdavidsson@dhcp-190254.eduroam.chalmers.se>
2015-04-28 15:28:12 +0200

    Human klassen klar

--- MosquitoSimulator/src/main/java/edu/cth/mosquito/mosquitosimulator/Human.java
+++ MosquitoSimulator/src/main/java/edu/cth/mosquito/mosquitosimulator/Human.java
@@ -12,6 +12,18 @@ public class Human extends newSolidObject{
     private float blood;
     
     public Human(){
-        
+        blood = 100;   
+    }
+    
+    public float getBlood(){
+        return blood;
+    }
+    
+    public void decreaseBlood(float amount){
+        blood -= amount;
+    }
+    
+    public void increaseBlood(float amount){
+        blood += amount;
     }
 }
