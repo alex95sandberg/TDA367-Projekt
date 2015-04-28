86dfc5ac322336579f7dc69cca51aa6d23c392b2
Alexander Sandberg <alex95sandberg@gmail.com>
2015-04-28 15:32:40 +0200

    logik newSolidObject

--- MosquitoSimulator/src/main/java/edu/cth/mosquito/mosquitosimulator/newSolidObject.java
+++ MosquitoSimulator/src/main/java/edu/cth/mosquito/mosquitosimulator/newSolidObject.java
@@ -10,5 +10,36 @@ package edu.cth.mosquito.mosquitosimulator;
  */
 public class newSolidObject {
     private Position3D position;
+    private float width, heigth;
     
+    public newSolidObject(){
+        position = new Position3D();
+    }
+    
+    public newSolidObject(Position3D position){
+        this.position = position;
+    }
+    
+    public newSolidObject(float width, float heigth){
+        this.width = width;
+        this.heigth = heigth;
+    }
+    
+    public newSolidObject(Position3D position, float width, float heigth){
+        this.position = position;
+        this.width = width;
+        this.heigth = heigth;
+    }
+    
+    private Position3D getPosition(){
+        return position;
+    }
+    
+    private float getWidth(){
+        return width;
+    }
+    
+    private float getHeigth(){
+        return heigth;
+    }
 }
