300756ac635750c2636900b836c9fd9cdfe9414e
Chewii <rasmusdavidsson@dhcp-191038.eduroam.chalmers.se>
2015-05-29 14:25:05 +0200

    final 2

---
+++ MosquitoSimulator/src/main/java/edu/cth/mosquito/core/Objective4.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cth.mosquito.core;
+
+/**
+ *
+ * @author rasmusdavidsson
+ */
+
+
+
+public class Objective4 implements Objectives {
+
+    private float progress = 0;
+    private static final int GOAL = 10;
+    private static final int REWARD = 60;
+    private static final String OBJECTIVETEXT = "Hold your energy at 100% for a total of 10 seconds\nReward 60 points";
+    
+    
+    public Objective4(){
+
+    
+    }
+    @Override
+    public float getObjectiveReward() {
+         return REWARD;
+    }
+
+    @Override
+    public float getObjectiveGoal() {
+
+        return GOAL;
+    }
+
+    @Override
+    public float getProgress() {  
+        return progress;
+    }
+
+    @Override
+    public String getObjectiveText() {
+        return OBJECTIVETEXT;
+    }
+
+    @Override
+    public void increaseProgress(float amount) {
+      progress += amount;  
+      if(progress >= GOAL)
+          progress = GOAL;
+          
+    }
+
+    @Override
+    public void setProgress(float amount) {
+        
+        progress = amount;
+    }
+}
