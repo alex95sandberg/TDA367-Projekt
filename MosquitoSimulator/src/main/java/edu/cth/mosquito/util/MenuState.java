6d3e518bbc241fccf852a415776eadf12b967e9c
Anton <anton.solback@gmail.com>
2015-05-30 15:13:57 +0200

    Kommenterade

--- MosquitoSimulator/src/main/java/edu/cth/mosquito/util/MenuState.java
+++ MosquitoSimulator/src/main/java/edu/cth/mosquito/util/MenuState.java
@@ -8,13 +8,14 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
- *
- * @author Anton
+ * A observer class that checks if its okay to initaite graphics
+ * @author Mosquito
  */
 public class MenuState implements PropertyChangeListener {
 
     private boolean initGraphics = false;
     
+    //When we get a propertyChange from an observed class thsi gets called.
     @Override
     public void propertyChange(PropertyChangeEvent evt){
         
